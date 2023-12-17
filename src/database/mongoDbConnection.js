105d173f22ddd62afa0b6ad9af0b8312b88a96af
Caio Camizão <caio_quimica2013@hotmail.com>
2023-12-17 14:52:40 -0300

    WIP: Refactoring from a CRUD to-do-list to a user registration CRUD

--- src/database/mongoDbConnection.js
+++ src/database/mongoDbConnection.js
@@ -1,12 +1,16 @@
-const mongoose = require('mongoose');
+const { mongoClient, MongoClient } = require('mongodb');
 
-const connectToDb = () => {
-  mongoose
-    .connect(
-      'mongodb+srv://root:admin@cluster0.kx6jzc4.mongodb.net/?retryWrites=true&w=majority'
-    )
-    .then(() => console.log('Connected to MongoDB'))
-    .catch((err) => console.log(err));
-};
+const MONGO_DB_URL = 'mongodb:/127.0.0.1:27017';
+const DB_NAME = 'user';
 
-module.exports = connectToDb;
+const connection = () => MongoClient.connect(MONGO_DB_URL, {
+    useNewParser: true,
+    useUnifiedTropology: true,
+})
+.then((conn) => conn.db(DB_NAME))
+.catch((err) => {
+    console.error(err);
+    process.exit(1);
+});
+
+module.exports = connection;
